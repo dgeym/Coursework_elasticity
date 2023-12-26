6f7b0dd6b5d5cf2f5938e0ecdd478fef60c6d17f
dgeym <152777806+dgeym@users.noreply.github.com>
2023-12-26 19:25:17 +0300

    Update createbody.py

--- metods/createbody.py
+++ metods/createbody.py
@@ -4,7 +4,7 @@ from models.materialbody import MaterialBody
 import numpy as np
 
 
-def rot(p: float, phi: float):
+def polar(p: float, phi: float):
     rx = np.cos(phi) - p * np.sin(phi)
     ry = p * np.cos(phi) + np.sin(phi)
     return rx, ry
@@ -14,7 +14,7 @@ def create_circle(body: BodyTraj, knots_num: int):
     arc = (2 * np.pi) / knots_num
     mat_body = MaterialBody([])
     for p in range(knots_num):
-        [x, y] = rot(body.radius, arc * p)
+        [x, y] = polar(body.radius, arc * p)
         x += body.center_x
         y += body.center_y
         new_point = MaterialPoint(p, x, y)
