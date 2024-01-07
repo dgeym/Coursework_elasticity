0c03c4063cee93f575c6c5f1d6358f0e32771b96
dgeym <152777806+dgeym@users.noreply.github.com>
2024-01-07 23:12:03 +0300

    Update runge_kutta.py

--- metods/runge_kutta.py
+++ metods/runge_kutta.py
@@ -3,10 +3,10 @@ from metods.def_func import f_x1, f_x2
 from models.bodytraj import PointTraj
 from models.materialpoint import MaterialPoint
 
-
-'n = 100'
-
-
+"""
+Реализация метода численного интегрирования Рунге-Кутта с использованием
+таблицы Бутчера, заданной в условии
+"""
 t = 0
 def runge_kutta(x_r, t, f):
     h = 0.01
@@ -27,20 +27,21 @@ def runge_kutta(x_r, t, f):
     return x_mass
 
 def integr_rungekutta(mat_point: MaterialPoint, times_num: int):
-    point_traj = PointTraj(mat_point.p_id, [], [])
+    point_traj = PointTraj(mat_point.p_id, [], []) 'Создание экземпляра PointTraj'
     h = 0.01
 
-    for i in range(100):
+    for i in range(100):                 'Заполнение массива времени, элементы с одинаковым промежутком h'
         point_traj.add_new_time(h*(i+1))
 
-    point_traj.add_new_position(mat_point)
-    x1 = point_traj.mat_point_tr[0].coord_x1
+    point_traj.add_new_position(mat_point) 'Добавление новой позиции точки с соотвествующим id'
+    x1 = point_traj.mat_point_tr[0].coord_x1 'За отсчетные x и y берутся координаты первых материальных точек '
+                                             '(в нашем случае - точек тела в недеформированном состоянии'
     y1 = point_traj.mat_point_tr[0].coord_x2
     for i in range(times_num-1):
-        x2 = runge_kutta(x1, point_traj.time_grid[i], f_x1)
+        x2 = runge_kutta(x1, point_traj.time_grid[i], f_x1) 'С учетом готового массива времени рассчитываются каждые следующие координаты заданным методом численного интегрирования'
         y2 = runge_kutta(y1, point_traj.time_grid[i], f_x2)
-        point_traj.mat_point_tr.append(MaterialPoint(mat_point.p_id, x2, y2))
-        x1 = x2
+        point_traj.mat_point_tr.append(MaterialPoint(mat_point.p_id, x2, y2)) 'Добавление каждой следующей материальной точки как экземпляра класса MaterialPoint'
+        x1 = x2                                                               'в массив - составляющую экземпляра point_traj'
         y1 = y2
 
     return point_traj
