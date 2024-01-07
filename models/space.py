f52a4b822fa9f0b3bb280a0ff57ef35f159dcb3d
dgeym <152777806+dgeym@users.noreply.github.com>
2024-01-07 21:44:46 +0300

    Update space.py

--- models/space.py
+++ models/space.py
@@ -4,6 +4,10 @@ from models.spacepoint import SpacePoint
 
 class Space:
     def __init__(self, b_id: int, sp_points: List[SpacePoint] = None):
+        #Инициализация переменных экземпляра класса: id материального тела и
+                                     #массива из принадлежащих ему материальных
+                                     #точек. Через каждый элемент массива могут быть вызваны переменные,
+                                     #связанные с соотвествующим экземпляром класса SpacePoint
         self.b_id = b_id
         self.sp_points = sp_points
 
