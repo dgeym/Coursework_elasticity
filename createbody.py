7919e9333d50dedf0ebb45b1daec56653c3a620d
dgeym <152777806+dgeym@users.noreply.github.com>
2024-01-07 22:01:16 +0300

    Update createbody.py

--- createbody.py
+++ createbody.py
@@ -5,14 +5,30 @@ import numpy as np
 
 
 def rot(x: float, y: float, r: float):
+    """
+    Функция перевода из полярных координат в декартовы
+    (для удобства построения набора материальных точек,
+    из которых состоит материальное тело)
+
+    :param p: полярный радиус
+    :param phi: полярный угол
+    :return: rx - координата по x, ry - координата по y
+    """
     rx = (x * np.cos(r)) - (y * np.sin(r))
     ry = (y * np.cos(r)) + (x * np.sin(r))
     return rx, ry
 
 
 def create_circle(body: BodyTraj, knots_num: int):
+    """
+    Функция создания массива точек, принадлежащему материальному телу
+
+    :param body: материальное тело
+    :param knots_num: узловые точки для шага
+    :return:
+    """
     arc = (2 * np.pi) / knots_num
-    mat_body = MaterialBody(1, [])
+    mat_body = MaterialBody([]) 'Создание экземпляра класса MaterialBody'
     for p in range(knots_num):
         [x, y] = rot(0, body.radius, arc * p)
         x += body.center_x
