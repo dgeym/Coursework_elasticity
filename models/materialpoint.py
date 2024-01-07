2c2e570b202d774a1cadf68dede19077f0408aef
dgeym <152777806+dgeym@users.noreply.github.com>
2024-01-07 21:41:44 +0300

    Update materialpoint.py

--- models/materialpoint.py
+++ models/materialpoint.py
@@ -1,5 +1,18 @@
 class MaterialPoint:
+    """
+    Класс материальной точки
+    """
     def __init__(self, p_id: int, coord_x1=None, coord_x2=None, vel1=None, vel2=None, time=None):
+        """
+        Конструктор класса материальной точки. Инициализирует переменные класса.
+
+        :param p_id: id материальной точки
+        :param coord_x1: первая координата
+        :param coord_x2: вторая координата
+        :param vel1: первая координата скорости
+        :param vel2: вторая координата скорости
+        :param time: момент инициализации, в который рассматриваются местоположение и скорость
+        """
         self.p_id = p_id
         self.coord_x1 = coord_x1
         self.coord_x2 = coord_x2
