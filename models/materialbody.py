74c3abe0cdea7acfdb1f780fb3e0d36b12ae298e
dgeym <152777806+dgeym@users.noreply.github.com>
2024-01-07 21:43:11 +0300

    Update materialbody.py

--- models/materialbody.py
+++ models/materialbody.py
@@ -3,9 +3,23 @@ from models.materialpoint import MaterialPoint
 
 
 class MaterialBody:
+    """
+    Класс материального тела
+    """
     def __init__(self, b_id: int, mat_points: List[MaterialPoint] = None):
+        """
+        Конструктор класса материального тела. Инициализирует переменные класса.
+
+        :param b_id: id материального тела
+        :param mat_points: массив принадлежащих ему материальных точек
+        """
         self.b_id = b_id
         self.mat_points = mat_points
 
     def add_new_point(self, new_point: MaterialPoint):
+        """
+        Функция, добавляющая в массив материальных точек данного тела новую материальную точку
+
+        :param new_point: добавляемая точка
+        """
         self.mat_points.append(new_point)
